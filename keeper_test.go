dd0081c40230f432b3cac0df51668721c3d3802b
Tedja <agunghariaditedja@yahoo.com>
2025-06-16 15:59:08 +0700

    feature: support INCRBY and DECRBY (#58)
    Co-authored-by: Agung Hariadi Tedja <agung.tedja@kumparan.com>

--- keeper_test.go
+++ keeper_test.go
@@ -415,6 +415,68 @@ func TestIncreaseCachedValueByOne(t *testing.T) {
 	assert.EqualValues(t, 1, count)
 }
 
+func TestIncreaseValueBy(t *testing.T) {
+	// Initialize new cache keeper
+	k := NewKeeper()
+
+	m, err := miniredis.Run()
+	assert.NoError(t, err)
+
+	r := newRedisConn(m.Addr())
+	k.SetConnectionPool(r)
+	k.SetLockConnectionPool(r)
+
+	testKey := "increase-test"
+	_, mu, err := k.GetOrLock(testKey)
+	assert.NoError(t, err)
+
+	err = k.Store(mu, NewItem(testKey, 0))
+	assert.NoError(t, err)
+
+	err = k.IncreaseValueBy(testKey, 10)
+	assert.NoError(t, err)
+
+	reply, _, err := k.GetOrLock(testKey)
+	assert.NoError(t, err)
+
+	var count int64
+	bt, _ := reply.([]byte)
+	err = json.Unmarshal(bt, &count)
+	assert.NoError(t, err)
+	assert.EqualValues(t, 10, count)
+}
+
+func TestDecreaseValueBy(t *testing.T) {
+	// Initialize new cache keeper
+	k := NewKeeper()
+
+	m, err := miniredis.Run()
+	assert.NoError(t, err)
+
+	r := newRedisConn(m.Addr())
+	k.SetConnectionPool(r)
+	k.SetLockConnectionPool(r)
+
+	testKey := "decrease-test"
+	_, mu, err := k.GetOrLock(testKey)
+	assert.NoError(t, err)
+
+	err = k.Store(mu, NewItem(testKey, 1000))
+	assert.NoError(t, err)
+
+	err = k.DecreaseValueBy(testKey, 999)
+	assert.NoError(t, err)
+
+	reply, _, err := k.GetOrLock(testKey)
+	assert.NoError(t, err)
+
+	var count int64
+	bt, _ := reply.([]byte)
+	err = json.Unmarshal(bt, &count)
+	assert.NoError(t, err)
+	assert.EqualValues(t, 1, count)
+}
+
 func TestDeleteByKeys(t *testing.T) {
 	t.Run("success", func(t *testing.T) {
 		// Initialize new cache keeper
