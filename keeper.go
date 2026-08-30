626b3fbe8d2953fbbda2252ccd7c9e9e724c48a4
Francisco <francisco.francisco@kumparan.com>
2026-08-30 19:55:19 +0700

    ah2

--- keeper.go
+++ keeper.go
@@ -542,7 +542,9 @@ func GetMultipleOrLoad[T any](
 			}
 			missing = append(missing, key)
 		}
-		fmt.Println("CACHER COK, Missing:", len(missing))
+		if len(missing) > 0 {
+			fmt.Println("CACHER COK, Missing:", len(missing))
+		}
 		if len(missing) == 0 {
 			break
 		}
@@ -561,7 +563,9 @@ func GetMultipleOrLoad[T any](
 			locked = append(locked, key)
 			mutexes = append(mutexes, mu)
 		}
-		fmt.Println("CACHER COK, Locked:", len(locked))
+		if len(locked) > 0 {
+			fmt.Println("CACHER COK, Locked:", len(locked))
+		}
 		if len(locked) > 0 {
 			loaderCallCount++
 			values, err := loader(ctx, utils.MapValuesToOrderedSlice(identifierByKey, locked))
@@ -579,7 +583,9 @@ func GetMultipleOrLoad[T any](
 				cacheItems = append(cacheItems, NewItem(key, value))
 				result[key] = value
 			}
-			fmt.Println("CACHER COK, stored:", len(cacheItems))
+			if len(cacheItems) > 0 {
+				fmt.Println("CACHER COK, stored:", len(cacheItems))
+			}
 			if err := k.StoreMultiWithoutBlocking(cacheItems); err != nil {
 				logrus.Error(err)
 			}
